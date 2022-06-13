e85e60350a11410a15f95220ca89db40b4234197
tanmayagrawal2000 <53507833+tanmayagrawal2000@users.noreply.github.com>
2022-06-14 03:10:34 +0530

    Update start.php

--- start.php
+++ start.php
@@ -1,5 +1,5 @@
 <?php
-#!$output=shell_exec('bash tracescript.sh');
+$output=shell_exec('bash tracescript.sh');
 echo "<h2>PACKETS SAVED</h2>";
 echo "<br>";
 echo "<button style='background-color: #4CAF50; border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer' onclick=" . "document.location='http://localhost/input.html'" . ">View Packets</button>";
